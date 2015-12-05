e5993702093389ee3766ab5d03ae0774a5b6f2b2
John Louros <jlouros@live.com>
2015-12-04 22:06:57 -0500

    adding multi-subnet-failover test apps using .Net 4.5 and 4.6.1

---
+++ TestMultiSubnetFailover-461/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Net;
+using System.Threading;
+
+namespace TestMultiSubnetFailover_461
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This application will run 'SELECT SERVERPROPERTY('MachineName')' against the server specified in the configuration settings.\n");
+
+            // list hosts
+            string dbFqdn = ConfigurationManager.AppSettings["DatabaseFullyQualifiedDomainName"];
+            Console.WriteLine("Gathering list of IP addresses associated with '{0}'", dbFqdn);
+
+            foreach (IPAddress add in Dns.GetHostEntry(dbFqdn).AddressList)
+            {
+                Console.WriteLine("   {0}", add);
+            }
+
+            Console.WriteLine("\nExecution options: ");
+            Console.WriteLine("  [e] runs a single test");
+            Console.WriteLine("  [r] runs long pooling test execution, press [Ctrl] + [c] to exit");
+            Console.WriteLine("  [q] exit the application");
+            Console.WriteLine();
+
+            ConsoleKeyInfo userSel;
+            do
+            {
+                userSel = Console.ReadKey(true);
+
+                FailoverTester failTest = new FailoverTester();
+
+                if (userSel.Key == ConsoleKey.E)
+                {
+                    failTest.RunTests();
+                }
+
+                if (userSel.Key == ConsoleKey.R)
+                {
+                    // never ending loop, use [Ctrl] + [c] to terminate the program
+                    while (true)
+                    {
+                        failTest.RunTests();
+                    }
+                }
+
+            } while (userSel.Key != ConsoleKey.Q);
+
+
+        }
+    }
+
+    public class FailoverTester
+    {
+        static string motorboat { get { return ConfigurationManager.ConnectionStrings["motorboat"].ConnectionString; } }
+        static int poolingSleepTime { get { return int.Parse(ConfigurationManager.AppSettings["SleepTimeForLongPooling"]); } }
+
+        public object TestRequest()
+        {
+            // get machine name from SQL Server
+            using (SqlConnection sqlConn = new SqlConnection(motorboat))
+            using (SqlCommand sqlCmd = new SqlCommand("SELECT SERVERPROPERTY('MachineName')", sqlConn))
+            {
+                sqlConn.Open();
+
+                return sqlCmd.ExecuteScalar();
+            }
+        }
+
+        public void RunTests()
+        {
+            try
+            {
+                Console.WriteLine(">> '{0}'", TestRequest());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Thread.Sleep(poolingSleepTime);
+        }
+    }
+}
---
+++ scripts/Toggle-SqlServerService.ps1
@@ -0,0 +1,67 @@
+﻿# quick and dirt script to Start/Stop MsSqlServer
+# needs to run on a MWE environment (maybe in the same VLan)
+
+# parameters
+$dbHostname = 'msfqamockdb.mwe.local'
+$serviceName = 'MSSQLSERVER'
+
+
+# waits for either 'Running' or 'Stopped' state for the given service name
+function Wait-ForStableStatus {
+	param(
+		[string] $srvName,
+		[string] $pcName
+	)
+
+	$done = $false;
+	do {
+		$srv = Get-Service -Name $srvName -ComputerName $pcName
+
+		if($srv.Status -eq 'Running' -or $srv.Status -eq 'Stopped' ) { 
+			$done = $true 
+		}
+
+		if($done -eq $false) {
+			Write-Host "waiting for stable service status on '$pcName'"
+			Start-Sleep -Seconds 1
+		}
+		
+	} while ($done -eq $false)
+}
+
+
+
+$ips = @()
+$dbs = @()
+[System.Net.Dns]::Resolve($dbHostname).AddressList | % { $ips += $_.IPAddressToString }
+
+# DNS based failover check
+if($ips.Count -lt 2) { Write-Error "Can't run this script with less than 2 IPs associated with the domain name!" -ErrorAction Stop }
+
+$ips | % { $dbs += Get-WmiObject Win32_Service -Filter "Name='$serviceName'" -ComputerName $_ } 
+
+$ips | % { Wait-ForStableStatus $serviceName $_ }
+
+
+
+
+
+if(($dbs | ? { $_.State -eq 'Stopped' } | measure).Count -lt 1) {
+	$dbs[0].StartService() | Out-Null
+	Wait-ForStableStatus $serviceName $dbs[0].PSComputerName
+}
+
+if(($dbs | ? { $_.State -eq 'Running' } | measure).Count -lt 1) {
+	$dbs[0].StopService() | Out-Null
+	Wait-ForStableStatus $serviceName $dbs[0].PSComputerName
+}
+
+
+$dbs | ? { $_.State -eq 'Stopped' } | % { $_.StartService() | Out-Null }
+
+$dbs | %{ Wait-ForStableStatus $serviceName $_.PSComputerName } 
+
+$dbs | ? { $_.State -eq 'Running' } | % { $_.StopService() | Out-Null }
+
+
+$ips | % { Get-Service -Name $serviceName -ComputerName $_ } 
