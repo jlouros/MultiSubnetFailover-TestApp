904497a00f356d7157760bf44e9221c10eaef980
John Louros <jlouros@live.com>
2015-12-04 23:04:09 -0500

    adding missing configurations

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# MultiSubnetFailover-TestApp
-Applications to test System.Data.SqlClient MultiSubnetFailover
+# Multi-subnet-failover test applications
+
+Applications to test [System.Data].SqlClient 'MultiSubnetFailover'.
+There's .Net 4.5 and .Net 4.6.1 versions.
+The code is exactly the same, just targeting different .Net versions.
+Modify 'App.config' with settings that match your environment.
+'App.Config' contains the following settings:
+ - 'DatabaseHostName': DNS entry targeting your database environment. Should contain at least 2 associated IPs
+ - 'SleepTimeForLongPooling': time the application will wait between SQL requests on a long pooling test (in milliseconds)
+ - 'dbConnString' (connection string): for .Net 4.5 'MultisubnetFailover=true' must be set. On .Net 4.6.1 that setting is not required
+ 
+ Additionally, under the scripts folder you can find a PowerShell script that toggles SQL Server service state for the provided database host name.
+ 
+ You can read more about this project at http://johnlouros.com/blog/leveraging-multi-subnet-failover
--- TestMultiSubnetFailover-45/Program.cs
+++ TestMultiSubnetFailover-45/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Net;
 using System.Threading;
 
@@ -8,22 +9,26 @@ namespace TestMultiSubnetFailover_45
 {
     class Program
     {
+        static string dbHostname = ConfigurationManager.AppSettings["DatabaseHostName"];
+        static int poolingSleepTime { get { return int.Parse(ConfigurationManager.AppSettings["SleepTimeForLongPooling"]); } }
+
         static void Main(string[] args)
         {
+            FailoverTester failTest = new FailoverTester();
+
             Console.WriteLine("This application will run 'SELECT SERVERPROPERTY('MachineName')' against the server specified in the configuration settings.\n");
 
-            // list hosts
-            string dbFqdn = ConfigurationManager.AppSettings["DatabaseFullyQualifiedDomainName"];
-            Console.WriteLine("Gathering list of IP addresses associated with '{0}'", dbFqdn);
+            // list IPs from the database hostname
+            Console.WriteLine("Gathering list of IP addresses associated with '{0}'", dbHostname);
 
-            foreach (IPAddress add in Dns.GetHostEntry(dbFqdn).AddressList)
+            foreach (IPAddress add in Dns.GetHostEntry(dbHostname).AddressList)
             {
                 Console.WriteLine("   {0}", add);
             }
 
             Console.WriteLine("\nExecution options: ");
-            Console.WriteLine("  [e] runs a single test");
-            Console.WriteLine("  [r] runs long pooling test execution, press [Ctrl] + [c] to exit");
+            Console.WriteLine("  [e] run a single test");
+            Console.WriteLine("  [r] run long pooling test execution");
             Console.WriteLine("  [q] exit the application");
             Console.WriteLine();
 
@@ -32,37 +37,57 @@ namespace TestMultiSubnetFailover_45
             {
                 userSel = Console.ReadKey(true);
 
-                FailoverTester failTest = new FailoverTester();
-
-                if (userSel.Key == ConsoleKey.E)
+                switch (userSel.Key)
                 {
-                    failTest.RunTests();
-                }
-
-                if (userSel.Key == ConsoleKey.R)
-                {
-                    // never ending loop, use [Ctrl] + [c] to terminate the program
-                    while (true)
-                    {
+                    case ConsoleKey.E:
+                        Console.WriteLine("running a single test...");
                         failTest.RunTests();
-                    }
+                        break;
+                    case ConsoleKey.R:
+                        Console.WriteLine("running long pooling tests... Press [r] to stop!");
+                        LongPoolAction(failTest.RunTests, ConsoleKey.R);
+                        break;
                 }
 
             } while (userSel.Key != ConsoleKey.Q);
 
+            Console.WriteLine("Bye!");
+        }
+
+        static void LongPoolAction(Action act, ConsoleKey stopKey)
+        {
+            bool runningTests = true;
+            while (runningTests)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    if (key.Key == stopKey)
+                    {
+                        Console.WriteLine("stopping...");
+                        runningTests = false;
+                    }
+                }
+
+                if (runningTests)
+                {
+                    // perform the requested action
+                    act();
+                    Thread.Sleep(poolingSleepTime);
+                }
+            }
 
         }
     }
 
     public class FailoverTester
     {
-        static string motorboat { get { return ConfigurationManager.ConnectionStrings["motorboat"].ConnectionString; } }
-        static int poolingSleepTime { get { return int.Parse(ConfigurationManager.AppSettings["SleepTimeForLongPooling"]); } }
-
+        static string dbConnString { get { return ConfigurationManager.ConnectionStrings["dbConnString"].ConnectionString; } }
+        
         public object TestRequest()
         {
             // get machine name from SQL Server
-            using (SqlConnection sqlConn = new SqlConnection(motorboat))
+            using (SqlConnection sqlConn = new SqlConnection(dbConnString))
             using (SqlCommand sqlCmd = new SqlCommand("SELECT SERVERPROPERTY('MachineName')", sqlConn))
             {
                 sqlConn.Open();
@@ -75,14 +100,19 @@ namespace TestMultiSubnetFailover_45
         {
             try
             {
-                Console.WriteLine(">> '{0}'", TestRequest());
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                object result = TestRequest();
+
+                sw.Stop();
+
+                Console.WriteLine(">> '{0}'; elapsed: '{1}' seconds", result, sw.Elapsed.TotalSeconds);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-
-            Thread.Sleep(poolingSleepTime);
         }
     }
 }
